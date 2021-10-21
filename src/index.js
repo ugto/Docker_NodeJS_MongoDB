ca96eafbb1307eb65811d4a4e82e964d251f97f5
Tarsicio Ramos L <t.ramos@ugto.mx>
2021-10-20 23:56:21 -0500

    se agrago nodeJs, mongoDB conectado con mongoose y mongo-express como ide grafico

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+require('./database');
+
+app.use(require('./routes/index'))
+
+app.listen(3000);
+console.log('SERVIDOR EN PUERTO',3000);
